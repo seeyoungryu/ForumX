9ead6e1c4229058bd26b524243d7bb97bdb92b45
seeyoungryu <seayeux@gmail.com>
2024-09-24 13:07:44 +0900

    Initial commit

---
+++ src/test/java/com/example/forumx/ForumXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forumx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumXApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
