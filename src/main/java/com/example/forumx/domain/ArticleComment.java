cea27da5e1c71616a2da16a7cfeef20db3070141
seeyoungryu <seayeux@gmail.com>
2024-10-04 16:10:55 +0900

    ArticleComment.java 필드값 수정

--- src/main/java/com/example/forumx/domain/ArticleComment.java
+++ src/main/java/com/example/forumx/domain/ArticleComment.java
@@ -4,12 +4,13 @@ import java.time.LocalDateTime;
 
 public class ArticleComment {
 
-    public long articleId;
-    public String content;
+    private Long id;
+    private Article article; //게시글 (ID)
+    private String content; //본문
 
     //메타데이터
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt; //생성일시
+    private String createdBy; //생성자
+    private LocalDateTime modifiedAt; //수정일시
+    private String modifiedBy; //수정자
 }
