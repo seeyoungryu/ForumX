f64837a8aa7b38561ffeed4f58dd7aaf9e951fe7
seeyoungryu <seayeux@gmail.com>
2024-10-05 23:46:20 +0900

    멀티 데이터소스 설정 분리

--- src/test/java/com/example/forumx/ForumXApplicationTests.java
+++ src/test/java/com/example/forumx/ForumXApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.forumx;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // test 프로파일 활성화
 class ForumXApplicationTests {
 
     @Test
