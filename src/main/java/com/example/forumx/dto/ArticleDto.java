161c5bb0bfcee00cf4daeb2f7ae9473f1ca04ea6
seeyoungryu <seayeux@gmail.com>
2024-10-06 19:13:08 +0900

    dto 임시

---
+++ src/main/java/com/example/forumx/dto/ArticleDto.java
@@ -0,0 +1,14 @@
+package com.example.forumx.dto;
+
+import java.time.LocalDateTime;
+
+public class ArticleDto {
+
+    String title;
+    String content;
+    String hashtag;
+    LocalDateTime createAt;
+    LocalDateTime updateAt;
+    LocalDateTime modifiedAt;
+    String modifiedBy;
+}
