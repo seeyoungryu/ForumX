11012aa1b5ca48636c75fd62e16de281d8c3884f
seeyoungryu <seayeux@gmail.com>
2024-10-08 15:10:01 +0900

    add CRUD method in Test class

--- src/test/java/com/example/forumx/repository/JpaRepositoryTest.java
+++ src/test/java/com/example/forumx/repository/JpaRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.forumx.repository;
 
 import com.example.forumx.config.JpaConfig;
+import com.example.forumx.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,25 @@ class JpaRepositoryTest {
     @Test
     void givenTestData_whenSelecting_thenWorksFine() {          //given_when_then 테스트 패턴
         articleRepository.findAll(); // 쿼리를 확인하기 위한 예시
+    }
+
+    @DisplayName("insert test")
+    @Test
+    void givenNewArticle_whenInserting_thenWorksFine() {
+        articleRepository.save(Article.of("New Title", "New Content", "New Hashtag"));
+    }
+
+    @DisplayName("update test")
+    @Test
+    void givenExistingArticle_whenUpdating_thenWorksFine() {
+        Article article = articleRepository.findById(1L).get(); // 간단하게 조회
+        article.setHashtag("#UpdatedHashtag");
+        articleRepository.save(article);
+    }
+
+    @DisplayName("delete test")
+    @Test
+    void givenExistingArticle_whenDeleting_thenWorksFine() {
+        articleRepository.deleteById(1L);
     }
 }
