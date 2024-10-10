af8fbc7e9d48a13a2edac3e9c16bbaefc8f3ced8
seeyoungryu <seayeux@gmail.com>
2024-10-10 15:58:14 +0900

    중복 필드 추출 - AuditingFields 완료

--- src/main/java/com/example/forumx/domain/AuditingFields.java
+++ src/main/java/com/example/forumx/domain/AuditingFields.java
@@ -2,29 +2,45 @@ package com.example.forumx.domain;
 
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
+@Getter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass //JPA 표준 애너테이션임.
 public class AuditingFields {
 
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt; //생성일시
+
     @CreatedBy
-    @Column(nullable = false, length = 100)
+    @Column(nullable = false, length = 100, updatable = false)
     private String createdBy;        //생성자
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedAt;//수정일시
+
     @LastModifiedBy
     @Column(nullable = false, length = 100)
     private String modifiedBy;       //수정자
 
 }
+
+
+//인덱스는 현재 클래스로 추출하기 어려움. (복잡함?)
