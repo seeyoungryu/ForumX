4b1d3cd523c48baf5920d84c8c4e86db0c4f4527
seeyoungryu <seayeux@gmail.com>
2024-10-11 14:31:40 +0900

    Repository에 @RepositoryRestResource 어노테이션 추가 (SpringDataRest 테스트를 위함)

--- src/main/java/com/example/forumx/repository/ArticleRepository.java
+++ src/main/java/com/example/forumx/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.example.forumx.repository;
 
 import com.example.forumx.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
 /* <레파지토리를 인터페이스로 사용하는 이유 관련>
@@ -11,6 +12,8 @@ JpaRepository를 인터페이스로 구현하여 ArticleRepository 를 선언하
 그럼 Spring Data JPA가 자동으로 필요한 메서드를 생성하고, 기본 CRUD 기능을 사용할 수 있습니다.
 기본 CRUD 메서드(save, findById, delete 등)는 자동으로 제공됩니다.
  */
+
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
 
